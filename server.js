7484c9e9e949f7cc11a5de308e1b0eb5bdf13c0f
Rochak Jain <rochakjain71@gmail.com>
2026-01-19 23:28:19 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { MongoClient, Collection } = require('mongodb');
+var cors = require('cors')
+
+ 
+
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+dotenv.config()
+
+//connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+
+//database name
+const dbName = 'passop';
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(cors())
+
+client.connect();
+
+//get all the passwords
+app.get("/", async (req, res) => {
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.find({}).toArray();
+    res.json(findResult)
+})
+//save a password
+app.post("/", async (req, res) => {
+    const password=req.body;
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.insertOne(password);
+    res.send({success:true,result:findResult});
+})
+//delete a pass by ID
+app.delete("/", async (req, res) => {
+    const password=req.body;
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.deleteOne(password);
+    res.send({success:true,result:findResult});
+})
+
+
+app.listen(port, () => {
+    console.log(`listening to the http://localhost:${port}`)
+})
